dc3bf74cefb56ba3780ad09de78473fc16bbcb43
abdelilah-fllh <125102837+abdelilah-fllh@users.noreply.github.com>
2024-05-04 20:34:52 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto Condensed', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
