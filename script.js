2d432b8270495e564af9181408ae356d00324795
abdelilah-fllh <125102837+abdelilah-fllh@users.noreply.github.com>
2024-05-09 05:46:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let el=document.getElementById('menu');
+
+function menu(){
+    if(el.style.display === "none")
+        el.style.display = "block";
+    else
+        el.style.display = "none";
+}
