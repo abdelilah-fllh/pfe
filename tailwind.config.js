ddac8886d1992da6e9b39515001662615e1842b4
abdelilah-fl <abdelilahfh02@gmail.com>
2024-05-22 11:39:11 +0200

    v1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js}'],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {
-      fontFamily: {
-        'roboto': ['Roboto Condensed', 'sans-serif']
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
